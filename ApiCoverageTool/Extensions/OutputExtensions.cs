22e30758ba5335439587d6a62b627674acb8bd05
Andrey Shvets <sas@vava.cars>
2022-04-11 23:03:25 +0300

    Add ToCsv support

---
+++ ApiCoverageTool/Extensions/OutputExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Linq;
+using ApiCoverageTool.Models;
+using CsvHelper;
+
+namespace ApiCoverageTool.Extensions
+{
+    public static class OutputExtensions
+    {
+        public static void ToCsvFile(this MappedApiResult mappedApiResult, string path)
+        {
+            if (File.Exists(path))
+                File.Delete(path);
+
+            if (mappedApiResult is null || !mappedApiResult.EndpointsMapping.Any())
+            {
+                File.WriteAllText(path, string.Empty);
+                return;
+            }
+
+            var combinedList = mappedApiResult.EndpointsMapping
+                .OrderByDescending(m => m.Value.Any())
+                .Select(m => new { Method = $"{m.Key.RestMethod} {m.Key.Path}", TestsCount = m.Value.Count })
+                .ToList();
+
+            using var writer = new StreamWriter(path);
+            using var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture);
+
+            csvWriter.WriteRecords(combinedList);
+        }
+    }
+}
